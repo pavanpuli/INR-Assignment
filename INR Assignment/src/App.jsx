b1da93988fa0beac248a73765cf04a575fa13202
PULI PAVAN TEJA GOUD <95956726+pavanpuli@users.noreply.github.com>
2022-07-11 15:43:47 +0530

    Add files via upload

---
+++ INR Assignment/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Homepage } from './Components/Homepage'
+import { Loginpage } from './Components/Loginpage'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Homepage/>
+      {/* <Loginpage/> */}
+    </div>
+  )
+}
+
+export default App
